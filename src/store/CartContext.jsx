f5a164ce7b60ca07588a64099985d634208c27ef
stillxthahn <lxthanh235@gmail.com>
2023-11-14 02:35:50 +0700

    !!!5. Getting Started with Cart Context & Reducer

---
+++ src/store/CartContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useReducer } from "react";
+const CartContext = createContext({
+  items: [],
+  addItem: (item) => {},
+  removeItem: (id) => {},
+});
+
+function cartReducer(state, action) {
+  if (action.type === "ADD_ITEM") {
+    const existingCartItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const updatedItems = [...state.items];
+    if (existingCartItemIndex > -1) {
+      const existingCartItem = state.items[existingCartItemIndex];
+      const updatedItem = {
+        ...existingCartItem,
+        quantity: existingCartItem.quantity + 1,
+      };
+      updatedItems[existingCartItem] = updatedItem;
+    } else {
+      updatedItems.push({ ...action.item, quantity: 1 });
+    }
+
+    return { ...state, items: updatedItems };
+  }
+  if (action.type === "REMOVE_ITEM") {
+    // ... remove an item from the state
+  }
+  return state;
+}
+
+export function CartContextProvider({ children }) {
+  useReducer(cartReducer, { items: [] });
+  return <CartContext.Provider>{children}</CartContext.Provider>;
+}
+
+export default CartContext;
