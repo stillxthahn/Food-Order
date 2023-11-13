cb03bbfa877b8dec2f115b088dae03d84f166be2
stillxthahn <lxthanh235@gmail.com>
2023-11-14 00:57:36 +0700

    1. FETCHING MEALS

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import Header from "../components/Header.jsx";
+import Meals from "../components/Meals.jsx";
 function App() {
   return (
     <>
       <Header />
+      <Meals />
     </>
   );
 }
