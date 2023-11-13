2af389c307bc10d5058f186c2f14e12e2378a588
stillxthahn <lxthanh235@gmail.com>
2023-11-14 00:27:44 +0700

    FIRST COMPONENT

---
+++ components/Header.jsx
@@ -0,0 +1,14 @@
+import logoImg from "../src/assets/logo.jpg";
+export default function Header() {
+  return (
+    <header id="main-header">
+      <div id="title">
+        <img src={logoImg} alt="A restaurant" />
+        <h1>ReactFood</h1>
+      </div>
+      <nav>
+        <button>Cart (0)</button>
+      </nav>
+    </header>
+  );
+}
